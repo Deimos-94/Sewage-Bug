b1008563a7f7c05f98674dc5653616c368f5cdc7
Ulasik <ulasik@cystep.de>
2023-12-06 14:52:25 +0100

    Player movement and default gravity changed
    Momentum based character movement. Much higher default gravity set, due to high resolution.

--- ScriptPlayer.gd
+++ ScriptPlayer.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 750.0
+const JUMP_VELOCITY = -1800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,7 +10,7 @@ var coyote = false
 var coyoteTime = 0.150 # Seconds
 var lastFloorContact = 0.0
 var jumpBufferMax = 0.5
-var jumpBuffer = 999
+var jumpBuffer = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -29,18 +29,20 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and lastFloorContact < coyoteTime:
 		velocity.y = JUMP_VELOCITY
 		lastFloorContact = 999
-	elif jumpBuffer >= 0:
-		velocity.y = JUMP_VELOCITY
-		lastFloorContact = 999
-	else:
-		jumpBuffer = jumpBufferMax
+	#elif jumpBuffer >= 0:
+		#velocity.y = JUMP_VELOCITY
+		#lastFloorContact = 999
+	#else:
+		#jumpBuffer = jumpBufferMax
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Accelerating based movement.
+	# Weird behavior: moving opposite of momentum makes moving more slippery than not mmoving
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
-		velocity.x = direction * SPEED
+		#velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, 25)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 40)
 
 	move_and_slide()
